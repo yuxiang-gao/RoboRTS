76cf92b8f7aed8422a55634ec2f753da49e7b10c
amamamahmood <amahmo11@jhu.edu>
2019-05-17 20:23:14 +0000

    Update reload_action.h

--- roborts_decision/include/hop_behaviour/reload_action.h
+++ roborts_decision/include/hop_behaviour/reload_action.h
@@ -18,8 +18,18 @@ public:
              Blackboard::Ptr &blackboard) : ActionNode("goal_action", blackboard),
                                             chassis_executor_(chassis_executor) {}
 
-  void OnInitialize()
+    void OnInitialize()
   {
+        reload_goal_.header.frame_id = "map";
+        reload_goal_.pose.orientation.x = 0;
+        reload_goal_.pose.orientation.y = 0;
+        reload_goal_.pose.orientation.z = 0;
+        reload_goal_.pose.orientation.w = 1;
+        reload_goal_.pose.position.x = 0;
+        reload_goal_.pose.position.y = 0;
+        reload_goal_.pose.position.z = 0;
+        reload_goal_ = true;
+
   }
 
   void OnTerminate(BehaviorState state)
@@ -32,11 +42,13 @@ public:
     auto executor_state = chassis_executor_->Update();
     if (executor_state != BehaviorState::RUNNING)
     {
-      if (blackboard_->IsNewGoal())
+      /*if (blackboard_->IsNewGoal())
       {
         chassis_executor_->Execute(blackboard_->GetGoal());
         return BehaviourState::SUCCESS;
-      }
+      }*/
+        chassis_executor_->Execute(blackboard_->GetReloadGoal());
+        return BehaviourState::SUCCESS;
     }
     return BehaviorState::RUNNING;
   }
