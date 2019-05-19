f7faafacf961cb06721273d9ea13775166aa56f6
Yuxiang Gao <davidgogh96@gmail.com>
2019-05-19 12:52:32 +0000

    script

--- roborts_bringup/scripts/upstart/max_performance.sh
+++ roborts_bringup/scripts/upstart/max_performance.sh
@@ -1,8 +1,9 @@
-#/usr/bash
-sleep 10
-echo dji | sudo -S sudo  "/home/dji/jetson_clocks.sh"
+#!/bin/bash
+bash -c "modprobe uvcvideo nodrop=1 timeout=6000 quirks=0x80"
+
+echo nvidia | sudo -S sudo  "/home/nvidia/jetson_clocks.sh"
 echo "Max performance"
 # disable wifi power saving
 
-echo dji | sudo -S sudo iw dev wlan0 set power_save off
+echo nvidia | sudo -S sudo iw dev wlan0 set power_save off
 echo "Set power_save off "
