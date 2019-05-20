277ea32cd8e18605c869e38a9835435e52b1c024
Yuxiang Gao <yuxianggao@jhu.edu>
2019-05-19 21:01:29 -0400

    update setup script

---
+++ roborts_bringup/scripts/upstart/add_swap.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+sudo fallocate -l 4G /swapfile
+sudo chmod 600 /swapfile
+sudo mkswap /swapfile
+sudo swapon /swapfile
+sudo cp /etc/fstab /etc/fstab.bak
+echo '/swapfile none swap sw 0 0' | sudo tee -a /etc/fstab
+# lower swappiness
+sudo sysctl vm.swappiness=10
