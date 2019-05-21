5050caeb11320f16e5424450ed8dfa4f1bd2eaac
Yuxiang Gao <yuxianggao@jhu.edu>
2019-05-21 09:44:40 -0400

    decision tree

--- roborts_decision/src/hop_decision_tree.cpp
+++ roborts_decision/src/hop_decision_tree.cpp
@@ -9,16 +9,23 @@ int main(int argc, char **argv)
   auto blackboard = std::make_shared<Blackboard>(full_path);
 
   auto tree_manager = std::make_shared<BehaviorTreeManager>(blackboard);
-  tree_manager->AddCompositeNodes({"selector_patrol", "sequence_countdown"});
+  tree_manager->AddCompositeNodes({"selector_patrol", "sequence_countdown", "parallel_engage"});
 
   tree_manager->Connect("condition_countdown", "sequence_countdown");
   tree_manager->Connect("condition_game_start", "selector_patrol");
   tree_manager->Connect("condition_reload", "action_reload");
   tree_manager->Connect("condition_bonus", "action_bonus");
-  tree_manager->Connect("condition_enemy_detected", "action_chase");
+  tree_manager->Connect("condition_enemy_detected", "parallel_engage");
 
-  tree_manager->Connect("selector_patrol", {"condition_reload", "condition_bonus", "condition_enemy_detected", "action_patrol"});
-  tree_manager->Connect("sequence_countdown", {"action_countdown", "action_back_boot_area", "condition_game_start"});
+  tree_manager->Connect("selector_patrol", {"condition_reload",
+                                            "condition_bonus",
+                                            "condition_enemy_detected",
+                                            "action_patrol"});
+  tree_manager->Connect("sequence_countdown", {"action_countdown",
+                                               "action_back_boot_area",
+                                               "condition_game_start"});
+  tree_manager->Connect("parallel_engage", {"action_shoot",
+                                            "action_chase"});
 
   tree_manager->Run("condition_countdown");
 
